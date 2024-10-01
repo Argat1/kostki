1bdb465851a7f4e36df550176be7d6596b9749b6
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-01 10:19:05 +0200

    Dodaj pliki projektów.

---
+++ kostki/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace kostki
+{
+    internal class Program
+    {
+        void Zagraj()
+        {
+            int ilosck = 3;
+            Console.WriteLine("Ile kostek chcesz rzucić?(3-10)");
+            ilosck =  int.Parse(Console.ReadLine());
+            if(ilosck > 2 && ilosck < 11) {
+            
+            }
+            else
+            {
+                Zagraj();
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
